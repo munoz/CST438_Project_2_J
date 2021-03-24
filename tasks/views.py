da79e9dd2c981675bdeeafb9d8d1fa8092e3dda3
yelinj <yjoh@csumb.edu>
2021-03-24 06:26:33 -0700

    added titles, extended base in 'viewUsers'

--- tasks/views.py
+++ tasks/views.py
@@ -8,19 +8,21 @@ from .forms import *
 
 # Create your views here.
 
+# def index(request):
+#     tasks = Task.objects.all()
+
+#     form = TaskForm()
+
+#     if request.method == 'POST':
+#         form = TaskForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#         return redirect('/')
+
+#     context = {'tasks':tasks, 'form':form}
+#     return render(request, 'tasks/list.html', context)
 def index(request):
-    tasks = Task.objects.all()
-
-    form = TaskForm()
-
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('/')
-
-    context = {'tasks':tasks, 'form':form}
-    return render(request, 'tasks/list.html', context)
+    return render(request, 'tasks/home.html')
 
 @login_required
 def viewItems(response, id):
